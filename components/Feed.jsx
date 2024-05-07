9310cb708549680fed46b9d2c12dbf0c24649d63
askeelad <khairulbashar19@gmail.com>
2024-05-07 16:03:36 +0600

    revalidate added

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -50,7 +50,7 @@ const Feed = () => {
 
   const fetchPosts = async () => {
     console.log("enter fetch");
-    const response = await fetch("/api/prompt", { revalidate: 10 });
+    const response = await fetch("/api/prompt", { next: { revalidate: 10 } });
     const data = await response.json();
     setPosts(data);
     setSeacrhedPosts(data);
